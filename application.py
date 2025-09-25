ec9e89aca9fc562225a2aa9e2838ab7cf5bbf193
Kelvin Murimi Njeru <148711587+kelvoBraavos@users.noreply.github.com>
2025-09-25 18:28:17 +0300

    Update application.py

--- application.py
+++ application.py
@@ -28,7 +28,7 @@ welcome = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AWS Elastic Beanstalk - Docker Sample Application V3</title>
+    <title>AWS Elastic Beanstalk - Docker Sample Application V4</title>
     <style>
         :root {
             --aws-squid-ink: #232f3e;
